e37519c969294e8c3d81847c6146c6c40a55b8f7
nlee07c-sudo <nlee07c@gmail.com>
2026-03-22 23:45:40 -0700

    k

--- labs/lab_1/lab_1d.py
+++ labs/lab_1/lab_1d.py
@@ -1,29 +1,11 @@
-"""
-lab_1d.py
-
-Given an array of integers `nums` and an integer `target`, return indices of the two numbers such that they add up to `target`.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-Derived from LeetCode problem: https://leetcode.com/problems/two-sum/ (leetcode easy)
-"""
 
 # TODO: Find and resolve the bug in the following implementation. Create unit tests to verify your fix.
 def two_sum(nums: list[int], target: int) -> list[int]:
-    """
-    Function that takes in a list of integers and a target integer, and returns the indices of the two numbers that add up to the target.
 
-    Args:
-        nums (list[int]): List of integers.
-        target (int): Target integer.
-    
-    Returns:
-        list[int]: Indices of the two numbers that add up to the target.
-    """
 
     num_to_index = {}
     for index, num in enumerate(nums):
-        complement = target + num
+        complement = target - num
         if complement in num_to_index:
             return [num_to_index[complement], index]
         num_to_index[num] = index
---
+++ tests/tests_1d.py
@@ -0,0 +1,17 @@
+
+from labs.lab_1.lab_1d import two_sum
+
+def test_basic_case():
+    assert two_sum([2, 7, 11, 15], 9) == [0, 1]
+
+def test_different_order():
+    assert two_sum([3, 2, 4], 6) == [1, 2]
+
+def test_duplicates():
+    assert two_sum([3, 3], 6) == [0, 1]
+
+def test_negative_numbers():
+    assert two_sum([-1, -2, -3, -4, -5], -8) == [2, 4]
+
+def test_no_solution():
+    assert two_sum([1, 2, 3], 10) == []
