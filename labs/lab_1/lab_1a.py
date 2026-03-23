2ef8461d2b949c95272af6b36ebb64cf3bc194d2
nlee07c-sudo <nlee07c@gmail.com>
2026-03-22 20:13:37 -0700

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("hello everyone")
 if __name__ == "__main__":
     main()
